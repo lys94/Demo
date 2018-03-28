bfbdebc5f1260b8cc6dff10f777eb7b7b8fd64f4
lys94 <31873307+lys94@users.noreply.github.com>
2018-03-28 16:47:37 +0800

    upload

---
+++ js/test.js
@@ -0,0 +1,22 @@
+// var a = 1;
+// var q = 2;
+
+// function test () {
+// 	switch (a) {
+//         case 1:
+//         black;
+// 	}
+// }
+
+// (a === 1) ?  表达式1:表达式2;
+var td = function () {
+	var a = 111;
+	console.log(a);
+
+}
+
+function bb (td) {
+	console.log(td);
+}
+
+bb();
